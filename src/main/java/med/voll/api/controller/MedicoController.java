f9d8933839c7bb305272b29cca0750638549e8ff
PJJunio <119122468+PJJunio@users.noreply.github.com>
2025-04-10 19:30:17 -0300

    Atualizando o MedicoController para utilizar as funcoes do Jpa de .save

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,10 @@
 package med.voll.api.controller;
 
-import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import med.voll.api.paciente.DadosPaciente;
 import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,16 +18,10 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
 
-    @Autowired
-    private PacienteRepository pacienteRepository;
-
     @PostMapping
     public void cadastrarMedico(@RequestBody DadosCadastroMedico dadosCadastroMedico){
         medicoRepository.save(new Medico(dadosCadastroMedico));
 
     }
 
-    public void cadastrarPaciente(@RequestBody DadosPaciente dadosPaciente){
-        pacienteRepository.save(new Paciente(dadosPaciente));
-    }
 }
