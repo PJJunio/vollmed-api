479345dda266189da2662dea9552bb6c1b6b2ae7
PJJunio <119122468+PJJunio@users.noreply.github.com>
2025-04-10 18:54:27 -0300

    Criacao dos DTOs para a interpretacao dos dados recebidos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,13 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import med.voll.api.paciente.DadosPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     @PostMapping
     public void cadastrarMedico(@RequestBody DadosCadastroMedico dadosCadastroMedico){
-        System.out.println(dadosCadastroMedico);
+        medicoRepository.save(new Medico(dadosCadastroMedico));
+
+    }
+
+    public void cadastrarPaciente(@RequestBody DadosPaciente dadosPaciente){
+        pacienteRepository.save(new Paciente(dadosPaciente));
     }
 }
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -3,5 +3,4 @@ package med.voll.api.medico;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
-
 }
