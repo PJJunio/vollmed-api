2e68faab76a62549b7ea61b5452020b7778d1573
PJJunio <119122468+PJJunio@users.noreply.github.com>
2025-04-10 19:30:03 -0300

    Atualizando o PacienteController para utilizar as funcoes do Jpa de .save

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.paciente.DadosPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     @PostMapping
-    public void cadastrarPaciente (@RequestBody DadosPaciente dadosPaciente){
-        System.out.println(dadosPaciente);
+    public void cadastrarPaciente(@RequestBody DadosPaciente dadosPaciente){
+        pacienteRepository.save(new Paciente(dadosPaciente));
     }
 }
